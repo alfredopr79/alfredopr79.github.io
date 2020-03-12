1322a6a3bdf5d0f879fe4d80d1407afbfa15ec53
Alfredo Peña Ramos <alfredopea@hotmail.com>
2020-03-12 12:13:52 -0600

    TSP and Simulated Annealing

---
+++ TSP_SimulatedAnnealing/AdvTec_Simulated_Annealing_V4.c
@@ -0,0 +1,175 @@
+/*Traveling Salesman Problem + Simulated*/
+//Instance Sources: http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsp/index.html
+//Profile: http://linkedin.com/in/alfredopea
+//Alfredo Peña Ramos /PHD Student
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int n;
+int indexBestSol=0;
+int* Fitness;
+int** sol;
+int** instance;
+double PopulationSize=1;//Change if you want to calculate with more sol
+
+int** DynamicMemory2D(int n, int m);
+int CalculateFitness(int i);
+void CreatePermutations(int i);
+void StaticPermutation(int i);
+void SimAnnealing(int i);
+
+int main(int args,char** arg){
+    //put files in same code directory, if you want to add more instances to evaluate just modify array
+
+
+    FILE *file = fopen("29ciudades.txt", "r");
+    long start, end; 
+    int i,j,temp;
+
+    //Change it, if you want permanent Permutations Values
+    srand(128);
+
+    if(file == NULL){
+        printf("Read Error");
+    } 
+    fscanf(file,"%d",&n);
+    
+    //Allocate Dynamic Memory 1D, 2D Arrays.
+    Fitness = (int*)malloc(PopulationSize*sizeof(int));
+    
+    instance = DynamicMemory2D(n,n);
+    sol = DynamicMemory2D(PopulationSize,n);
+    
+    //Read Instance
+    for (i = 0; i < n; i++){
+        for (j = 0; j < n; j++){
+            fscanf(file,"%d",&instance[i][j]);
+        }
+    }
+    fclose(file);
+    
+    //Clear Array
+    for (i = 0; i < PopulationSize; i++){
+        Fitness[i] = 0;
+    }
+
+    start = clock();
+
+    //Create Population
+        for (i = 0; i < PopulationSize; i++){
+            //CreatePermutations(i);//If you want more solutions uncomment
+            StaticPermutation(i);//Solution like 0,1,2,3...n
+            Fitness[i] = CalculateFitness(i);
+            SimAnnealing(i);
+            if (Fitness[i] < Fitness[indexBestSol]){
+                indexBestSol = i;
+            }
+        }
+    end = clock();
+    double time = end - start;
+
+//Print Simulated Annealing Algorithm       
+        
+    for (i = 0; i < PopulationSize; i++){
+        printf("Fitness: %i\n",Fitness[i]);
+        printf("Solution: ");
+        for (j = 0; j < n-1; j++){                
+            printf("%i, ",sol[i][j]);
+        }
+        printf("%i\n",sol[i][j]);
+    }
+        printf("--------------------------------------------------------------------------------------------------------------------\n");
+        printf("\n  Best Solution: [%i]  Exec Time: %f\n",Fitness[indexBestSol],(time)/CLOCKS_PER_SEC);
+        printf("\n-------------------------------------------------------------------------------------------------------------------\n");
+        system("pause");
+}
+
+void CreatePermutations(int i){
+    int x,y,k,r,temp;
+    int Random[n];
+        for (k = 0; k < n; k++) {  // fill
+            Random[k] = k;
+        }
+        for (k = 0; k < n-1; k++) {  //shuffle
+            r = rand() % (n-k)+k;
+            temp = Random[k];
+            Random[k] = Random[r];
+            Random[r] = temp;
+        }
+        for (y = 0; y < n; y++){    //insert
+            sol[i][y] = Random[y];
+        }
+}
+
+void StaticPermutation(int i){//Permutation like 0,1,2,3...n
+    int x,y,k,r,temp;
+        for (y = 0; y < n; y++){    
+            sol[i][y] = y;
+        }
+}
+
+//Calculate Fitness Population
+int CalculateFitness(int i){
+    int j, summcol;
+    summcol = 0;
+    for (j = 0; j < n-1; j++){
+        summcol += instance[sol[i][j]][sol[i][j+1]];
+    }
+    summcol += instance[sol[i][n-1]][sol[i][0]];
+    return(summcol);
+}
+
+//Simulated Annealing Algorithm
+void SimAnnealing(int i){
+    int j, Lold, Lnew, temp; 
+    double T;
+
+    for(T=180; T > 1;){
+        for(j = 2; j < n-2;j++){//If LinkNew better than LinkOld change with LinkNew
+            
+            double alpha = 0.99;
+            T *= alpha;
+
+            Lold =  instance[sol[i][j-2]][sol[i][j-1]];
+            Lold +=  instance[sol[i][j-1]][sol[i][j]];
+            Lold +=  instance[sol[i][j]][sol[i][(j+1)%n]];
+            
+            Lnew = instance[sol[i][j-2]][sol[i][j]];
+            Lnew += instance[sol[i][j]][sol[i][j-1]];
+            Lnew += instance[sol[i][j-1]][sol[i][(j+1)%n]];
+
+            if(Lnew < Lold){
+                Fitness[i] = Fitness[i] - Lold + Lnew;
+                temp = sol[i][j-1];
+                sol[i][j-1] = sol[i][j];
+                sol[i][j] = temp;
+            }
+            else{//If LinkNew not better then apply Boltzman function
+
+                double probability = (rand() / (double)RAND_MAX);
+                double bolztman = exp(-(Lnew-Lold)/T);
+
+                if (bolztman > probability){
+                    Fitness[i] = Fitness[i] - Lold + Lnew;
+                    temp = sol[i][j-1];
+                    sol[i][j-1] = sol[i][j];
+                    sol[i][j] = temp;
+                }
+            }
+        }
+    }
+}
+
+//Reserve Memory for 2D Arrays used in Voids - Black Magic
+int** DynamicMemory2D(int n, int m){
+    int **aux;
+    int i;
+    aux = (int**)malloc(n*sizeof(int*));
+    for(int i = 0; i < n; i++){
+        aux[i] = (int*)malloc(m*sizeof(int));
+    }
+  return(aux);
+}
